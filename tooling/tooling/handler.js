771ab5d1f3439f28f43935870ed2f61e37bc2c3a
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-08-10 00:09:51 -0300

    finished tooling structure with docker

--- tooling/tooling/handler.js
+++ tooling/tooling/handler.js
@@ -1,12 +1,26 @@
 'use strict';
 
+const AWS = require('aws-sdk')
+
+const host = process.env.LOCALSTACK_HOST || "localhost"
+const s3port = process.env.S3_PORT || "4566"
+const s3Config = {
+  s3ForcePathStyle: true,
+  endpoint: new AWS.Endpoint(
+    `http://${host}:${s3port}`
+  )
+}
+
+const S3 = new AWS.S3(s3Config)
+
 module.exports.hello = async (event) => {
+  const allBuckets = await S3.listBuckets().promise()
+  console.log('found', allBuckets)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! Your function executed successfully!!',
-        input: event,
+        allBuckets
       },
       null,
       2
